61070ff755bbaabc0703a077eb96a00f05d371ec
Facundo López Llopis <flopezllopis@itba.edu.ar>
2024-10-14 11:46:32 -0300

    tp9 tests y ej introductorios

---
+++ Guías/Guía_9/tp9_0.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void palotes(int cant) {
+    if (cant == 0) return;
+    printf("|");
+    palotes(cant-1);
+}
+
+int strlen(char *s) {
+    if (*s == 0) return 0;
+    return 1 + strlen(s+1);
+}
+
+int strchr(char *s, char c) {
+    if (*s == c) return 0;
+    return 1 + strchr(s+1, c);
+}
+
+int main(void) {
+    // Pruebas
+    return 0;
+}
